43f4feff0eff87c45f653dc353b85ec167d2a502
aliciaMA <a24aliciaMA@iessanclemente.net>
2024-11-20 17:41:31 +0100

    py

---
+++ Ejercicios/Boletin3_1/Ej1_1.py
@@ -0,0 +1,7 @@
+print("Vamos a sumar 2 numeros")
+num1 = int(input("Dame el primer numero "))
+num2 = int(input("Dame el segundo numero "))
+print(f"{num1} + {num2} = ", num1 + num2)
+
+print("Al reves es igual")
+print("Para que veas", f"{num2} + {num1} = ", num2 + num1)
